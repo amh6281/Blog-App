d4ae56a2e1d6525e8a6936f1aace75c7ba2f150b
amh6281 <amh6281@naver.com>
2023-09-09 09:44:24 +0900

    Theme에 따른 css 적용 2023.09.09

--- src/components/themeToggle/ThemeToggle.tsx
+++ src/components/themeToggle/ThemeToggle.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./themeToggle.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
+import { ThemeContextType } from "@/types/types";
 
 const ThemeToggle = () => {
+  const theme = useContext<ThemeContextType | undefined>(ThemeContext);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onClick={theme?.toggle}
+      style={
+        theme?.theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }
+    >
       <Image src="/moon.png" alt="moon" width={14} height={14} />
-      <div className={styles.ball}></div>
+      <div
+        className={styles.ball}
+        style={
+          theme?.theme === "dark"
+            ? { left: 1, background: "#0f172a" }
+            : { right: 1, background: "white" }
+        }
+      ></div>
       <Image src="/sun.png" alt="moon" width={14} height={14} />
     </div>
   );
