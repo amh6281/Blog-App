93e02da0e22d2286ed8313e76cc6cb8127ef9928
amh6281 <amh6281@naver.com>
2023-09-13 08:54:38 +0900

    CardList component design 2023.09.13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import CardList from "@/components/cardList/CardList";
+import styles from "./home.module.css";
 import CategoryList from "@/components/categoryList/CategoryList";
 import Featured from "@/components/featured/Featured";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Featured />
       <CategoryList />
-    </>
+      <div className={styles.content}>
+        <CardList />
+      </div>
+    </div>
   );
 }
