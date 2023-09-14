1803b7e3b8b6225f5cdb76cc3009215378a37761
amh6281 <amh6281@naver.com>
2023-09-14 10:22:38 +0900

    MenuCategories component design 2023.09.14

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
 import styles from "./menu.module.css";
-import Image from "next/image";
 import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
@@ -9,6 +8,9 @@ const Menu = () => {
       <h2 className={styles.subTitle}>What's hot</h2>
       <h1 className={styles.title}>Most Popular</h1>
       <MenuPosts withImage={false} />
+      <h2 className={styles.subTitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
       <h2 className={styles.subTitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
       <MenuPosts withImage={true} />
