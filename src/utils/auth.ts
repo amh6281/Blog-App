1cfee69780aad749a0a786384d026c0fba15b248
amh6281 <amh6281@naver.com>
2023-09-18 09:26:22 +0900

    Next-Auth를 이용한 AuthProvider 생성 및 wrapping 2023.09.18

---
+++ src/utils/auth.ts
@@ -0,0 +1,16 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+};
