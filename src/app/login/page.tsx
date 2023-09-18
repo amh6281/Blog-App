860a0658d8ca7c7d0a4f51a514f46f45bbe9ed74
amh6281 <amh6281@naver.com>
2023-09-18 09:51:24 +0900

    Login, Logout 기능 구현 2023.09.18
    Next-auth의 signIn, signOut을 이용하여 로그인, 로그아웃 기능을 구현한다.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useSession, signIn } from "next-auth/react";
 import styles from "./loginPage.module.css";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>구글 로그인</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          구글 로그인
+        </div>
         <div className={styles.socialButton}>깃허브 로그인</div>
         <div className={styles.socialButton}>페이스북 로그인</div>
       </div>
