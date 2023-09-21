84e3c4176fff9d32da0771cee1cb9bb0afc0b5bc
amh6281 <amh6281@naver.com>
2023-09-21 10:17:19 +0900

    Prisma-adapter를 이용한 사용자 관리 2023.09.21

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,11 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./connect";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
