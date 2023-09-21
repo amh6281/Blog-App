08394d24f60bd90ed3e9d1c1fb6add76fe38da6b
amh6281 <amh6281@naver.com>
2023-09-21 10:21:46 +0900

    Write page 인증 및 권한 관리 2023.09.21
    Status에 따른 권한 설정 및 페이지 리다이렉션

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -5,10 +5,23 @@ import styles from "./writePage.module.css";
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const WritePage = () => {
+  const { status } = useSession();
+
+  const router = useRouter();
+
   const [open, setOpen] = useState<boolean>(false);
   const [value, setValue] = useState<string>("");
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (status === "authenticated") {
+    router.push("/");
+  }
 
   return (
     <div className={styles.container}>
