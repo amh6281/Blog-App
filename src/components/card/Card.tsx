1da76e98f9e3b81f6010476d592010d526d294f9
amh6281 <amh6281@naver.com>
2023-09-27 09:39:12 +0900

    Post data fetch 2023.09.27

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
+import { PostType } from "@/types/types";
 
-const Card = () => {
+const Card = ({ key, item }: { key: string; item: PostType }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
         <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
@@ -14,7 +15,7 @@ const Card = () => {
           <span className={styles.category}>Coding</span>
         </div>
         <Link href="/">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1>{item.title}</h1>
         </Link>
         <p className={styles.desc}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -2,15 +2,28 @@ import Image from "next/image";
 import styles from "./cardList.module.css";
 import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
+import { PostType } from "@/types/types";
 
-const CardList = () => {
+const getData = async (page: number) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    throw new Error("실패");
+  }
+  return res.json();
+};
+
+const CardList = async ({ page }: { page: number }) => {
+  const data = await getData(page);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>최근 게시물</h1>
       <div className={styles.posts}>
-        <Card />
-        <Card />
-        <Card />
+        {data?.map((item: PostType) => (
+          <Card key={item._id} item={item} />
+        ))}
       </div>
       <Pagination />
     </div>
