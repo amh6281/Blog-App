5eb0247401609f190d5c86580ce483a6746ae210
amh6281 <amh6281@naver.com>
2023-09-29 20:30:47 +0900

    Posts pagination api 추가 2023.09.29

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,9 +2,23 @@ import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page");
+  const parsedPage = page ? parseInt(page, 10) : 1;
+
+  const POST_PER_PAGE = 2;
+
+  const query = {
+    take: POST_PER_PAGE,
+    skip: POST_PER_PAGE * (parsedPage - 1),
+  };
+
   try {
-    const posts = await prisma.post.findMany();
-    return new NextResponse(JSON.stringify(posts), { status: 200 });
+    const [posts, count] = await prisma.$transaction([
+      prisma.post.findMany(query),
+      prisma.post.count(),
+    ]);
+    return new NextResponse(JSON.stringify({ posts, count }), { status: 200 });
   } catch (err) {
     console.log(err);
     return new NextResponse(JSON.stringify({ message: "잘못된 접근입니다." }), {
