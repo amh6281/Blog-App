3915816a6f5290fa4845050255c5167d46ae1f38
amh6281 <amh6281@naver.com>
2023-09-30 12:15:44 +0900

    Get posts api category filter 추가 2023.09.30

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -3,20 +3,25 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   const { searchParams } = new URL(req.url);
+
   const page = searchParams.get("page");
   const parsedPage = page ? parseInt(page, 10) : 1;
+  const cat = searchParams.get("cat");
 
   const POST_PER_PAGE = 2;
 
   const query = {
     take: POST_PER_PAGE,
     skip: POST_PER_PAGE * (parsedPage - 1),
+    where: {
+      ...(cat && { catSlug: cat }),
+    },
   };
 
   try {
     const [posts, count] = await prisma.$transaction([
       prisma.post.findMany(query),
-      prisma.post.count(),
+      prisma.post.count({ where: query.where }),
     ]);
     return new NextResponse(JSON.stringify({ posts, count }), { status: 200 });
   } catch (err) {
