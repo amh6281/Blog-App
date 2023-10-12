5e15763f01a8d864947171eedd113bff1710dd75
amh6281 <amh6281@naver.com>
2023-10-12 09:02:51 +0900

    Create post api 생성 2023.10.12

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -24,6 +25,33 @@ export const GET = async (req: NextRequest) => {
       prisma.post.count({ where: query.where }),
     ]);
     return new NextResponse(JSON.stringify({ posts, count }), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(JSON.stringify({ message: "잘못된 접근입니다." }), {
+      status: 500,
+    });
+  }
+};
+
+// Create a Post
+export const POST = async (req: NextRequest) => {
+  const session = await getAuthSession(); // await를 사용하여 Promise 해결
+
+  if (!session || !session.user) {
+    return new NextResponse(
+      JSON.stringify({ message: "인증되지 않았습니다." }),
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const post = await prisma.post.create({
+      data: { ...body, userEmail: session.user.email },
+    });
+    return new NextResponse(JSON.stringify(post), { status: 200 });
   } catch (err) {
     console.log(err);
     return new NextResponse(JSON.stringify({ message: "잘못된 접근입니다." }), {
